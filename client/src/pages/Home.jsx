582e0f0e5429c5192be865ce0c20d04478827ae5
JOBA Razafindrasoa Genitah <genitahrazafindrasoa@gmail.com>
2026-03-03 08:53:22 +0300

    Correction de la page Home et des contrôles de saisie

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
     // MESSAGE DE BIENVENUE (MessageBox)
     if (isLoggedIn) {
         Swal.fire({
-          title: `Ravi de vous revoir, ${displayName} ! 👋`,
+          title: `Ravi de vous revoir, ${user.name || user.username || (user.user && user.user.name)} ! 👋`,
           text: "Prêt à découvrir nos nouvelles pépites pour votre intérieur ?",
           icon: 'success',
           toast: true,
