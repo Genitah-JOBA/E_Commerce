c52ee53d82e69bfcb3986c998de1472812d73949
JOBA Razafindrasoa Genitah <genitahrazafindrasoa@gmail.com>
2026-03-15 10:53:59 +0300

    Correction de l'attribut style en double dans Footer.tsx

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Force un nouveau hash
+        entryFileNames: `assets/[name]-[hash]-new.js`,
+        chunkFileNames: `assets/[name]-[hash]-new.js`,
+        assetFileNames: `assets/[name]-[hash]-new.[ext]`
+      }
+    }
+  }
 })
